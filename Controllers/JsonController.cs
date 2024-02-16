ab731dd0b3c2fd40a1c8c0d4e6fa33a514690cd2
Jason <ax6030@gmail.com>
2024-02-16 20:51:55 +0800

    add Youbike pagedList

--- Controllers/JsonController.cs
+++ Controllers/JsonController.cs
@@ -1,4 +1,5 @@
 ﻿using JsonDownload.Models;
+using PagedList;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,10 +13,20 @@ namespace JsonDownload.Controllers
     public class JsonController : Controller
     {
         // GET: Json
-        public async Task<ActionResult> Index()
+        public async Task<ActionResult> Index(int page = 1)
         {
             var model = await JsonService.GetDataAsync_Youbike();
-            return View(model);
+
+            //顯示第幾頁，當參數< 1時會設定為第1頁，避免不正確輸入
+            var currentPage = page < 1 ? 1 : page;
+
+            //每頁資料筆數
+            var pageSize = 20;
+
+            //將取得資料model轉成PagedList
+            var pageModel = model.OrderBy(m=>m.sno).ToPagedList(currentPage, pageSize);
+
+            return View(pageModel);
         }
 
         public async Task<ActionResult> GetPlace()
--- Models/Youbike.cs
+++ Models/Youbike.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Web;
 
@@ -8,23 +9,53 @@ namespace JsonDownload.Models
 
     public class Youbike
     {
+        [DisplayName("編號")]
         public string sno { get; set; }
+
+        [DisplayName("站點")]
         public string sna { get; set; }
+
+        [DisplayName("總車位")]
         public int tot { get; set; }
+
+        [DisplayName("可借車位")]
         public int sbi { get; set; }
+
+        [DisplayName("區域")]
         public string sarea { get; set; }
+
+        [DisplayName("資料更新時間")]
         public string mday { get; set; }
+
+        [DisplayName("緯度")]
         public float lat { get; set; }
+
+        [DisplayName("經度")]
         public float lng { get; set; }
+
+        [DisplayName("地址")]
         public string ar { get; set; }
+
+        [DisplayName("area")]
         public string sareaen { get; set; }
+
+        [DisplayName("name")]
         public string snaen { get; set; }
+
+        [DisplayName("address")]
         public string aren { get; set; }
+
+        [DisplayName("可還空位數")]
         public int bemp { get; set; }
+
         public string act { get; set; }
+
         public string srcUpdateTime { get; set; }
+
         public string updateTime { get; set; }
+
         public string infoTime { get; set; }
+
         public string infoDate { get; set; }
     }
 
