d4d43983167ab662c7672352ddfa47f77e284d5d
Jason <ax6030@gmail.com>
2024-02-17 11:47:53 +0800

    add TravelPlace and BusTaxi PageList

--- Controllers/JsonController.cs
+++ Controllers/JsonController.cs
@@ -2,26 +2,41 @@
 using PagedList;
 using System;
 using System.Collections.Generic;
+using System.Drawing.Printing;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.UI;
 using System.Web.UI.WebControls;
 
 namespace JsonDownload.Controllers
 {
     public class JsonController : Controller
     {
+        private int page = 1;
+        private int pageSize = 20;
+        private int currentPage;
+        public int CurrentPage 
+        { 
+            get => currentPage;
+            set
+            {
+                if (value <= 1)
+                    currentPage = 1;
+                else
+                    currentPage = value;
+            }
+        }
         // GET: Json
-        public async Task<ActionResult> Index(int page = 1)
+        public async Task<ActionResult> Index()
         {
             var model = await JsonService.GetDataAsync_Youbike();
 
-            //顯示第幾頁，當參數< 1時會設定為第1頁，避免不正確輸入
-            var currentPage = page < 1 ? 1 : page;
+            //顯示第幾頁，當參數< 1時會設定為第1頁，避免不正確輸入       
+            currentPage = page;
 
-            //每頁資料筆數
-            var pageSize = 20;
 
             //將取得資料model轉成PagedList
             var pageModel = model.OrderBy(m=>m.sno).ToPagedList(currentPage, pageSize);
@@ -32,14 +47,33 @@ namespace JsonDownload.Controllers
         public async Task<ActionResult> GetPlace()
         {
             var model = await JsonService.GetDataAsync_TravelPlace();
-            return View(model);
+
+            ////顯示第幾頁，當參數< 1時會設定為第1頁，避免不正確輸入         //原始寫法
+            //var currentPage = page < 1 ? 1 : page;
+
+            ////每頁資料筆數
+            //var pageSize = 20;
+
+            currentPage = page;
+            //將取得資料model轉成PagedList
+            var pageModel = model.OrderBy(m => m.Id).ToPagedList(currentPage, pageSize);
+
+            return View(pageModel);
         }
 
 
         public async Task<ActionResult> GetBusTaxi()
         {
+            
             var model = await JsonService.GetDataAsync_BusTaxi();
-            return View(model);
+            currentPage = page;
+
+            //將取得資料model轉成PagedList
+            var pageModel = model.OrderBy(m=>m.name).ToPagedList(currentPage, pageSize);
+
+            return View(pageModel);
         }
+
+        
     }
 }
